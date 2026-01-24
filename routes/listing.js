0939bde2bd39c85d72d20a71d95b713e5973afe3
Shivangi <shivangichaurasia1515@gmail.com>
2026-01-24 17:04:09 +0530

    Add flash messages to listing and review routes

--- routes/listing.js
+++ routes/listing.js
@@ -47,6 +47,11 @@ router.get("/:id",wrapAsync(async(req,res) =>{
 
     const listing=await Listing.findById(id).populate("reviews");
     
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
+     
     res.render("listings/show.ejs",{ listing });
 }));
 
@@ -60,6 +65,7 @@ router.post("/",validateListing, wrapAsync(async(req,res,next)=>{
         
         const newListing=new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success","New Listing Created!")
         res.redirect("/listings");
 
 }));
@@ -73,6 +79,11 @@ router.get("/:id/edit",wrapAsync(async(req,res)=>{
 
     const listing=await Listing.findById(id);
 
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
+
     res.render("listings/edit.ejs",{listing});
 
 }));
@@ -83,6 +94,7 @@ router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
 
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash("success","Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -92,6 +104,7 @@ router.delete("/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success","Listing Deleted!");
     res.redirect("/listings");
 
 }));
