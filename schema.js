3597af1137256751f28dacd8e2393de5e6681166
Shivangi <shivangichaurasia1515@gmail.com>
2026-02-04 23:31:41 +0530

    Update Joi Validation schema for Review rating

--- schema.js
+++ schema.js
@@ -12,13 +12,19 @@ module.exports.listingSchema=Joi.object({
     }).required()
 
 });
-module.exports.reviewSchema=Joi.object({
-    review:Joi.object({
-        rating:Joi.number().required().min(1).max(5),
-        comment:Joi.string().required(),
 
-
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number()
+            .required()
+            .min(1)
+            .max(5)
+            .messages({
+                "any.required": "Rating is required",
+                "number.base": "Rating must be a number",
+                "number.min": "Rating must be at least 1",
+                "number.max": "Rating cannot be more than 5"
+            }),
+        comment: Joi.string().required()
     }).required()
-
-
 })
